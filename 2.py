5eef1eeae56d3186359d36e22804cbd3d40cd967
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-15 19:54:08 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -10,3 +10,20 @@ print(df.sort_values('Name'))#ascending
 print(df.sort_values('Name',ascending= False))#descending
 print(df.sort_values(['Type 1','Speed'],ascending= [1,0]))#sorting on the basis of two column values
 
+#making changes to the data
+df['Total'] = df['HP'] + df['Speed']+df['Attack']  #to add new column
+df= df.drop(columns=['Total']) #to drop a column
+df['Total'] = df.iloc[:,4:10].sum(axis=1)#add new column where axis = 1 means add horizontally and 0 means add vertically
+
+#to rearrange the columns
+cols = list(df.columns)
+df = df[cols[0:4] + [cols[-1]]+cols[4:12]] 
+
+#exporting the data to desired format
+df.to_csv('modified.csv', index=False)
+
+df.to_excel('modified.xlsx', index=False)
+
+df.to_csv('modified.txt', index=False, sep='\t')
+
+
