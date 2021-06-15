09bc9ca2601fe88bc5008a173a3c84a447cd8b5c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-15 19:30:52 +0530

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,12 @@
+import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv') #to read csv file
+
+#describing data
+print(df.describe())#print the statistic details like mean,min,std
+
+#sorting data
+print(df.sort_values('Name'))#ascending
+print(df.sort_values('Name',ascending= False))#descending
+print(df.sort_values(['Type 1','Speed'],ascending= [1,0]))#sorting on the basis of two column values
 
