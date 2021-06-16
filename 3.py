b496e624f71765f6da4a16fbc82dd7427761fb1b
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-16 17:34:20 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -5,7 +5,7 @@ df = pd.read_csv('pokemon_data.csv') #to read csv file
 #filtering the data
 
 df.loc[df['Type 1']== 'Grass']
-df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) #filtered based om two conditions
+df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) #filtered based on two conditions
 
 
 
