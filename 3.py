b17ff7a497a99f35193b4270204f9fade3aadf14
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-16 20:48:55 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -15,6 +15,23 @@ print(df_new)
        
 
 #filtering data based on textual pattern(regex filtering)
+       
+df.loc[df['Name'].str.contains('Mega')]#filter rows that contains Mega
+df.loc[~df['Name'].str.contains('Mega')] #filter rows that does not contains Mega
+
+import re
+       
+df.loc[df['Type 1'].str.contains('fire|grass', flags=re.I, regex=True)] #this flag will ignore case
+df.loc[df['Name'].str.contains('^pi[a-z]*', flags=re.I, regex=True)]
+       
+#conditional changes
+   
+df.loc[df['Type 1']== 'Fire','Type 1'] = 'Flamer'
+df.loc[df['Type 1']== 'Fire','Legendary'] = True #parameter does not to be the same column
+df.loc[df['HP']<50,['Generation','Legendary']]= 'same value' #changing two columns at the same time with same value 
+df.loc[df['HP']<50,['Generation','Legendary']]= ['diff1','diff2] #changing two columns at the same time with different value 
+ 
+ 
 
 
 
