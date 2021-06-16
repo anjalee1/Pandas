b2faab3c5bd1f33213d9817b830b7b521501a4ad
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-16 16:31:51 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,1 +1,14 @@
+import pandas as pd
+
+df = pd.read_csv('pokemon_data.csv') #to read csv file
+
+#filtering the data
+
+df.loc[df['Type 1']== 'Grass']
+df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) #filtered based om two conditions
+
+
+
+
+
 
