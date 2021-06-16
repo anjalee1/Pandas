9084edcdf78705a285b6dff834ce773651c07427
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-16 19:16:42 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -6,6 +6,13 @@ df = pd.read_csv('pokemon_data.csv') #to read csv file
 
 df.loc[df['Type 1']== 'Grass']
 df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) #filtered based on two conditions
+df.loc[(df['Type 1']== 'Grass') | (df['Speed']== 80)]
+       
+#resetting index
+# df_new = df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) & (df['HP'] >70)].reset_index() #storing data to new data frame and resetting the index so that it will start from 0
+df_new = df.loc[(df['Type 1']== 'Grass') & (df['Speed']== 80) & (df['HP'] >70)].reset_index(drop=True)#use drop parameter to remove index from older dataframe
+print(df_new)
+       
 
 
 
