8c191f319e33c8320d9b016294663619207b4e03
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-17 13:17:17 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,1 +1,10 @@
+import pandas as pd
 
+df = pd.read_csv('pokemon_data.csv') #to read csv file
+
+#aggregrate statistics(group by)
+
+df.groupby(['Type 1']).mean().sort_values('Attack')
+df.groupby(['Type 1']).count()
+df.groupby(['Type 1']).sum()
+df.groupby(['Type 1','Type 2']).mean()#group by two parameters
