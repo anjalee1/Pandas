2326d5cb776f2e48f889ba2f4ab6da53ce29f05e
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-17 15:42:58 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -21,5 +21,18 @@ for df in pd.read_csv('pokemon_data.csv' , chunksize= 5):# reading data in chunk
     new_df =pd.concat([new_df,results])
     new_df = new_df.loc[: ,new_df.columns !='Type 1'] # stores all columns except 'Type 1'
     print(new_df)
+    
+#creating pandas dataframe
+
+df = pd.DataFrame(data=[[1,2,3],[4,5,6],[7,8,9]],index = ['row1','row2','row3'],columns=['col1','col2','col3'])
+print(df)
+
+lst=['tesla', 'carl sagan', 'einstein']
+df1 =pd.DataFrame(lst)#Creating dataframe using list
+print(df1)
+
+dct ={'Name':['Tom', 'nick', 'krish', 'jack'], 'Age':[20, 21, 19, 18]}
+df2 = pd.DataFrame(dct)#Creating dataframe using dictionary
+print(df2)
 
 
