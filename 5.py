39d74260d0f6ebd4a033f53283e01be9068f1dbf
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-18 13:33:45 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,16 @@
+import pandas as pd
 
+df = pd.read_csv('pokemon_data.csv',) #to read csv file
+
+#boolean indexing
+
+ #Accessing a DataFrame with a boolean index
+data = df.head(5)
+data.index=[True, False, True, False,True]
+data =data.drop(columns=['#'])
+print(data)
+print(data.loc[True])
+#In order to access a dataframe using .iloc[], we have to pass a boolean
+# value (True or False)  but iloc[] function accept only integer as argument so
+# it will throw an error if we pass  boolean  value (True or False) ]
+print(data.iloc[0:5])
