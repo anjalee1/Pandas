8940e81e9fa9fe6ac54461c8a149ee856a2dd5d0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-06-19 22:50:31 +0530

    Update 6.py

--- 6.py
+++ 6.py
@@ -1,1 +1,11 @@
+import pandas as pd
 
+df= pd.read_csv("pokemon_data.csv", usecols=['Name','Speed','Attack'],nrows=6)
+
+#add new column to existing dataframe
+
+new_col=['col1','col2','col3','col4','col5','col6']
+df['AU-col'] = new_col #method1
+df.insert(2, "Anjali", [1, 2, 3, 4,5,6], True) #method2- using insert()
+df=df.assign(Nickname = ['Bul', 'Ivy', 'Ven', 'venus','charm','charl'])#method3- using assign()
+print(df)
